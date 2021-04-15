3d39af53dfce7ff14d90630f78267390023da1e0
Doğukaan Kılıçarslan <40922278+darkbringer1@users.noreply.github.com>
2021-04-15 13:32:51 +0300

    Project 1 created (WeSplit)

---
+++ Project 1 WeSplit/Project 1 WeSplit/Project_1_WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_1_WeSplitApp.swift
+//  Project 1 WeSplit
+//
+//  Created by DarkBringer on 15.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct Project_1_WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
